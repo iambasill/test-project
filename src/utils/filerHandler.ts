7060d5269b7c8bbafe4ab2344e5ff9d8e72873e7
Basil <okwutebasil01@gmail.com>
2025-08-12 12:36:30 +0100

    edited

--- src/utils/filerHandler.ts
+++ src/utils/filerHandler.ts
@@ -4,12 +4,12 @@ export const fileHandler = (request:any) => {
     const files = request.files as Record<string, Express.Multer.File[]>;
     const fileData = Object.entries(files).map(([fileName, [file]]) => ({
       fileName,
-      url: file.path.toString(),
+      url: file.path,
         }));
     
         return fileData
 }
 
 export const fileController = (req:Request, res:Response) => {
-  
+
 }
