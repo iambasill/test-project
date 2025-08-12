b29b1048822bbf2d1c248c7c6ea27d270447e8b4
Basil <okwutebasil01@gmail.com>
2025-08-12 14:36:19 +0100

    edited

--- src/config/fileUpload.ts
+++ src/config/fileUpload.ts
@@ -18,6 +18,9 @@ let upload_path = path.join(__dirname, "../storage/document");
 // Configure storage
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
+    console.log("req: ", req)
+    console.log("file: ", file)
+
     cb(null, upload_path); 
   },
   filename: (req, file, cb) => {
