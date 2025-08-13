aeb78609699396cf9f3f2aee37896336ea7b2ea1
Basil <okwutebasil01@gmail.com>
2025-08-13 23:54:51 +0100

    edited

--- src/controller/inspectionController.ts
+++ src/controller/inspectionController.ts
@@ -16,6 +16,7 @@ export const createInspection = async (req:Request, res:Response) => {
         functionalInspections = [],
         documentLegalInspections = []
     } = req.body;
+
     const user: any = req.user
     const files = req.files || [];
 
