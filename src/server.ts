227a6ad3a0114e249b41bc69dc0c5bce02e5ffd3
Basil <okwutebasil01@gmail.com>
2025-08-14 07:05:41 +0100

    edited

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import { PrismaClient } from '../src/generated/prisma';
 
 export const prisma = new PrismaClient()
 const app = express()
+
 app.use(morganMiddleware);
 
 app.use(cors({
@@ -15,7 +16,9 @@ app.use(cors({
   // origin: 'http://your-frontend-domain.com',
   // credentials: true,
 }));
-app.use(express.json())
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use('/', rootRoute)
 
 
