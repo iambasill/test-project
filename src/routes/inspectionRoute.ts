32add330b6dc154ca62e4cb1a5f293b9279af69f
Basil <105466598+Ycaptn@users.noreply.github.com>
2025-08-16 14:52:04 +0100

    Update inspectionRoute.ts

--- src/routes/inspectionRoute.ts
+++ src/routes/inspectionRoute.ts
@@ -1,4 +1,4 @@
-6import  express  from "express";
+import  express  from "express";
 import { upload } from '../config/fileUpload';
 import { createInspection, getAllInspections, getInspectionById, updateInspection } from "../controller/inspectionController";
 import { authMiddleware } from "../middlewares/authMiddleware";
