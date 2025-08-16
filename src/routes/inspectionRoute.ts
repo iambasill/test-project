a29467cbbcf7d28696a80fe1556a1b419be9347b
Basil <105466598+Ycaptn@users.noreply.github.com>
2025-08-16 13:06:06 +0100

    Update inspectionRoute.ts

--- src/routes/inspectionRoute.ts
+++ src/routes/inspectionRoute.ts
@@ -1,4 +1,5 @@
 import  express  from "express";
+import { upload } from '../config/fileUpload';
 import { createInspection, getAllInspections, getInspectionById, updateInspection } from "../controller/inspectionController";
 import { authMiddleware } from "../middlewares/authMiddleware";
 
@@ -6,7 +7,7 @@ export const inspectionRouter = express.Router();
 
 inspectionRouter.get('/', authMiddleware, getAllInspections);
 inspectionRouter.get('/:id', authMiddleware, getInspectionById);
-inspectionRouter.post('/', authMiddleware, createInspection);
+inspectionRouter.post('/', authMiddleware, upload.none(), createInspection);
 inspectionRouter.put('/:id', authMiddleware, updateInspection);
 inspectionRouter.delete('/:id',authMiddleware,)
 
