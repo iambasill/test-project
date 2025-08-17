bcb442385d42f447642ee20d0b453e4ac3e9643b
Basil <okwutebasil01@gmail.com>
2025-08-17 21:05:59 +0100

    edited

--- src/routes/inspectionRoute.ts
+++ src/routes/inspectionRoute.ts
@@ -2,12 +2,13 @@ import  express  from "express";
 import { upload } from '../config/fileUpload';
 import { createInspection, getAllInspections, getInspectionById,  } from "../controller/inspectionController";
 import { authMiddleware } from "../middlewares/authMiddleware";
+import { UPLOAD_FIELDS } from "../schema/uploadsSchema";
 
 export const inspectionRouter = express.Router();
 
 inspectionRouter.get('/', authMiddleware, getAllInspections);
 inspectionRouter.get('/:id', authMiddleware, getInspectionById);
-inspectionRouter.post('/', authMiddleware, upload.none(), createInspection);
+inspectionRouter.post('/', authMiddleware, upload.fields(UPLOAD_FIELDS), createInspection);
 
 
 
