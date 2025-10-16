7e79b37aed05298b671676009a8ebd780e628771
Basil <okwutebasil01@gamil.com>
2025-10-16 11:11:25 +0100

    edited

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -23,7 +23,7 @@ export const authMiddleware = async(req: Request, _res: Response, next: NextFunc
     if (!decoded || typeof decoded !== 'object' || !('id' in decoded)) throw new unAuthorizedError("INVALID OR EXPIRED TOKEN");
 
     const {user}:any = await checkUser((decoded as any).id)
-    if (!user || user.status !== "ACTIVE") throw new unAuthorizedError("Access Denied")
+    if (!user || user.status?.toUpperCase() !== "ACTIVE") throw new unAuthorizedError("Access Denied")
     req.user = user
     req.token = token
     next();
