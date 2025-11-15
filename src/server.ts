d229f5edc8eae0f9177c37221562621d7a303ed3
Basil <okwutebasil01@gmail.com>
2025-11-15 10:14:19 +0100

    edited

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // CRITICAL: allow OPTIONS preflight
-app.options("*", cors(corsOptions));
+// app.options("*", cors(corsOptions));
 
 
 // ----------------------
